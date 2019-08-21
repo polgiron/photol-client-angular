2e69ddbfb444c4eb5aa8142c4bfb5a0f278ec0d5
polgiron <pol.giron@gmail.com>
2019-08-21 15:59:16 +0100

    Add suggested tags

--- src/app/components/tags/tags.component.ts
+++ src/app/components/tags/tags.component.ts
@@ -4,8 +4,6 @@ import { Tag } from 'src/app/models/tag.model';
 import { Image } from 'src/app/models/image.model';
 import { ImageService } from 'src/app/services/image.service';
 import { SettingsService } from 'src/app/services/settings.service';
-import { takeWhile } from 'rxjs/operators';
-import { Settings } from 'src/app/models/settings.model';
 
 @Component({
   selector: 'app-tags',
@@ -13,33 +11,40 @@ import { Settings } from 'src/app/models/settings.model';
   styleUrls: ['./tags.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TagsComponent implements OnInit, OnDestroy {
+export class TagsComponent implements OnInit {
   @Input() image: Image;
-  private _alive: boolean = true;
+  @Input() editMode: boolean = false;
   newTagValue: string = '';
   allTags: Tag[];
-  editMode: boolean = false;
+  suggestedTags: Tag[];
 
   constructor(
     private tagService: TagService,
     private imageService: ImageService,
-    private ref: ChangeDetectorRef,
-    private settingsService: SettingsService
+    private ref: ChangeDetectorRef
   ) { }
 
   ngOnInit() {
     // console.log(this.image.tags);
-
-    this.settingsService.settingsChannel()
-      .pipe(takeWhile(() => this._alive))
-      .subscribe((settings: Settings) => {
-        this.editMode = settings.editMode;
-        this.ref.markForCheck();
-      });
   }
 
   async onFocus() {
     this.allTags = await this.tagService.getAll();
+    this.updateSuggestedTags();
+    this.ref.markForCheck();
+  }
+
+  // onBlur() {
+  //   console.log('ON BLUR');
+  //   this.suggestedTags = null;
+  //   this.ref.markForCheck();
+  // }
+
+  updateSuggestedTags() {
+    this.suggestedTags = this.allTags.filter(tag => {
+      return !this.image.tags.find(imageTag => imageTag._id == tag._id);
+    });
+    this.ref.markForCheck();
   }
 
   async onKeyEnter() {
@@ -48,24 +53,10 @@ export class TagsComponent implements OnInit, OnDestroy {
 
       if (existingTag) {
         // Add tag to image
-        // console.log('tag already exists');
-
-        this.image.tags = this.image.tags.concat(existingTag);
-
-        this.imageService.update(this.image._id, {
-          tags: this.image.tags
-        });
-
+        this.addExistingTag(existingTag);
       } else {
         // Create tag and add to image
-        // console.log('create tag and add to image');
-
-        const newTag = await this.tagService.create({
-          value: this.newTagValue,
-          images: [this.image._id]
-        });
-
-        this.image.tags = this.image.tags.concat(newTag);
+        this.addNewTag(this.newTagValue);
       }
 
       this.newTagValue = '';
@@ -73,15 +64,30 @@ export class TagsComponent implements OnInit, OnDestroy {
     }
   }
 
+  addExistingTag(tag: Tag) {
+    this.image.tags = this.image.tags.concat(tag);
+    this.imageService.update(this.image._id, {
+      tags: this.image.tags
+    });
+    this.updateSuggestedTags();
+    this.ref.markForCheck();
+  }
+
+  async addNewTag(tag: string) {
+    const newTag = await this.tagService.create({
+      value: tag,
+      images: [this.image._id]
+    });
+    this.image.tags = this.image.tags.concat(newTag);
+    this.ref.markForCheck();
+  }
+
   removeTag(deleteTag: Tag) {
     this.image.tags = this.image.tags.filter(tag => tag._id != deleteTag._id);
     this.imageService.update(this.image._id, {
       tags: this.image.tags
     });
+    this.updateSuggestedTags();
     this.ref.markForCheck();
-  }
-
-  ngOnDestroy() {
-    this._alive = false;
   }
 }
